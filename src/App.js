0fa33c1eae9314bb0b9ee55c8fefcc19c2098d8b
pandaqueen4799 <58620160+pandaqueen4799@users.noreply.github.com>
2021-09-22 23:36:20 -0400

    add h1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather React Project App
+     <h1> Weather React Project App </h1>
     </div>
   );
 }
