dd1cf25ce62b50e86222c3dbd37b82df0c0c5354
pandaqueen4799 <58620160+pandaqueen4799@users.noreply.github.com>
2021-09-23 23:25:26 -0400

    added basic stylings

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-     <h1> Weather React Project App </h1>
      <Weather />
      <footer>
        This project was coded by{" "}
--- src/weather.js
+++ src/weather.js
@@ -5,19 +5,22 @@ export default function Weather() {
   return (
     <div className="weather">
       <form>
-          <div className="row">
-              <div className="col-9">
-        <input
-          type="search"
-          placeholder="Enter a city..."
-          className="form-control"
-        />
-        </div>
-        <div className="col-3">
-        <input type="submit" 
-        value="Search" 
-        className="btn btn-primary" />
-        </div>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
         </div>
       </form>
       <h1>New York</h1>
@@ -25,13 +28,17 @@ export default function Weather() {
         <li>Wed 07:00</li>
         <li>Ranning</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
-            alt="Rainng"
-          />
-          6°C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
+              alt="Rainng"
+            />
+
+            <span className="temp">6</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
