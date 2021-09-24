b42b536b285c3167b9b5a27d9a1e05fd9a35873f
pandaqueen4799 <58620160+pandaqueen4799@users.noreply.github.com>
2021-09-23 21:43:40 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
      <h1> Weather React Project App </h1>
+     <footer>
+       This project was coded by{" "}
+       Tessa D. A. {" "}
+       <a href="https://github.com/pandaqueen4799/projectweatherreactapp/tree/master/src"
+       target="_blank" rel="noreferrer">
+         open-sourced on GitHub
+       </a>
+     </footer>
+
+</div>
     </div>
   );
 }
