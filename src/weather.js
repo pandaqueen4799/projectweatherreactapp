1a7e74604398b4e24d40bf713c95947e1a2d3477
pandaqueen4799 <58620160+pandaqueen4799@users.noreply.github.com>
2021-09-24 19:52:22 -0400

    added date on weather

--- src/weather.js
+++ src/weather.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import FormattedDate from "./FormattedDate";
 import axios from "axios";
 import "./weather.css";
 
@@ -11,7 +12,7 @@ export default function Weather(props) {
           ready: true,
           temp: response.data.main.temp,
           humidity: response.data.main.humidity,
-          date: "Wed 07:00",
+          date: new Date(response.data.dt * 1000),
           description: response.data.weather[0].description,
           iconUrl: "https://ssl.gstatic.com/onebox/weather/64/rain_light.png",
           wind: response.data.wind.speed,
@@ -44,7 +45,9 @@ export default function Weather(props) {
       </form>
       <h1>{weatherData.city}</h1>
       <ul>
-        <li>{weatherData.date}</li>
+        <li>
+     <FormattedDate date={weatherData.date} />
+           </li>
         <li className="text-capitalize">{weatherData.description}</li>
       </ul>
       <div className="row mt-3">
@@ -69,7 +72,7 @@ export default function Weather(props) {
     </div>
   );
 } else {
-    const apiKey = "b19af43e62b4a3c076b585af4be814dd";
+    const apiKey = "91dfb81ac7d5ccde305914fa015a1604";
     let apiUrl =`http://api.openweathermap.org/data/2.5/weather?q=
     ${props.defaultCity}&appid=${apiKey}&units=metric`;
       axios.get(apiUrl).then(handleResponse);
@@ -77,3 +80,4 @@ export default function Weather(props) {
       return "Loading...";
 }
 }
+ 
