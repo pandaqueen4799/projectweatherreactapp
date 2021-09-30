b3311265a60dd34bf93d3c072774328d65cec2c9
pandaqueen4799 <58620160+pandaqueen4799@users.noreply.github.com>
2021-09-30 14:26:33 -0400

    added a link

--- src/weatherIcon.js
+++ src/weatherIcon.js
@@ -9,20 +9,19 @@ export default function WeatherIcon(props) {
     "02n": "PARTLY_CLOUDY_NIGHT",
     "03d": "CLOUDY ",
     "03n": "CLOUDY ",
-    "04d": "PARTLY_CLOUDY_DAY ",
-    "04n": "PARTLY_CLOUDY_NIGHT ",
+    "04d": "PARTLY_CLOUDY_DAY",
+    "04n": "PARTLY_CLOUDY_NIGHT",
     "09d": "RAIN",
     "09n": "RAIN",
-    "010d": "RAIN",
-    "010n": "RAIN",
-    "011d": "RAIN",
-    "011n": "RAIN",
-    "013d": "SNOW",
-    "013n": "SNOW",
+    "10d": "RAIN",
+    "10n": "RAIN",
+    "11d": "RAIN",
+    "11n": "RAIN",
+    "13d": "SNOW",
+    "13n": "SNOW",
     "050d": "FOG",
     "050n": "FOG",
   };
-  
 
   return (
     <ReactAnimatedWeather
